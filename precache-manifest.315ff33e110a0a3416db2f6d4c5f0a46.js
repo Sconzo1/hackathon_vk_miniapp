35a9ed8f4def8ba8708755e25e0e0ef8cd90638e
Simraki <Simraki@mail.ru>
2020-06-21 16:36:09 +0700

    Updates

---
+++ precache-manifest.315ff33e110a0a3416db2f6d4c5f0a46.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecefb3ef12f993c0b63d4fb1b4ba0dc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8d4ddcbd9d5c3fa69429",
+    "url": "./static/css/2.6bab1db4.chunk.css"
+  },
+  {
+    "revision": "8d4ddcbd9d5c3fa69429",
+    "url": "./static/js/2.8949f492.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.8949f492.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a8404b672747228a006",
+    "url": "./static/js/main.e2e45f6f.chunk.js"
+  },
+  {
+    "revision": "1c9b504a697469ddc397",
+    "url": "./static/js/runtime-main.7f5553f8.js"
+  },
+  {
+    "revision": "f6494a4afe7e059176dafc0e173a3af2",
+    "url": "./static/media/Buzza.f6494a4a.jpg"
+  },
+  {
+    "revision": "13ecb0122a9573a64e445f1c044d636e",
+    "url": "./static/media/Funchoza.13ecb012.jpg"
+  },
+  {
+    "revision": "d5c4e89deb4f8ac7a3492b100272911a",
+    "url": "./static/media/Oreo.d5c4e89d.jpg"
+  },
+  {
+    "revision": "5abcaaf6a327965097a5b6a1f816bade",
+    "url": "./static/media/Pancake.5abcaaf6.jpg"
+  },
+  {
+    "revision": "0a82b2c296c675d366783108d515581e",
+    "url": "./static/media/Pig.0a82b2c2.jpg"
+  },
+  {
+    "revision": "bcbe3ba281fadb2bcaee2ab15cf934a6",
+    "url": "./static/media/RedSea.bcbe3ba2.jpg"
+  }
+]);
